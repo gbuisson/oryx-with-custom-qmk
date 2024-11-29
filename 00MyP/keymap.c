39884b1bd6ce2ebd677ff66b8baffa520954e6c8
Guillaume Buisson <guillaume@gbuisson.com>
2024-11-29 16:27:33 +0100

    toggle autocorrect on left trigger

--- 00MyP/keymap.c
+++ 00MyP/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
     KC_TRANSPARENT, MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),MT(MOD_LSFT, KC_F),KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           MT(MOD_LSFT, KC_J),MT(MOD_LCTL, KC_K),MT(MOD_LALT, KC_L),MT(MOD_LGUI, KC_QUOTE),KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_Z,           MT(MOD_RALT, KC_X),KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       MT(MOD_RALT, KC_DOT),KC_SLASH,       KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_TRANSPARENT, LT(3,KC_ESCAPE),LT(2,KC_TAB),   LT(1,KC_SPACE), KC_TRANSPARENT,                                                                                                 ST_MACRO_0,     LT(5,KC_ENTER), LT(4,KC_BSPC),  LT(6,KC_DELETE),KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, LT(3,KC_ESCAPE),LT(2,KC_TAB),   LT(1,KC_SPACE), AC_TOGG,                                                                                                 ST_MACRO_0,     LT(5,KC_ENTER), LT(4,KC_BSPC),  LT(6,KC_DELETE),KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [1] = LAYOUT_moonlander(
